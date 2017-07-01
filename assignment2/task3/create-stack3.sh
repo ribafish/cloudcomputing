e3ec94bf9b2017aaca0aaa2231c966c5101053d3
Gasper Kojek <gasper.kojek@gmail.com>
2017-07-01 20:58:32 +0200

    Almost working, now it fails at building not at "compiling" the heat script

--- assignment2/task3/create-stack3.sh
+++ assignment2/task3/create-stack3.sh
@@ -1,7 +1,7 @@
 openstack stack create -t server.yaml assignment2-task3-stack \
 --parameter "name=assignment2-task3-vm" \
---parameter "router=cc17-router" \
---parameter "server=cc17-server" \
---parameter "network=cc17-net"	\
---parameter "subnet=cc17-subnet" \
+--parameter "router=cc17-23-router" \
+--parameter "server=cc17-23-server" \
+--parameter "network=cc17-23-net"	\
+--parameter "subnet=cc17-23-subnet" \
 --parameter "key_pair=group06key" \
