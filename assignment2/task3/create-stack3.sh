4b75fee83eab7e09064368c6e9a74a4cd766d593
Gasper Kojek <gasper.kojek@gmail.com>
2017-07-01 16:54:57 +0200

    Fixing create stack3.sh

--- assignment2/task3/create-stack3.sh
+++ assignment2/task3/create-stack3.sh
@@ -1,5 +1,7 @@
 openstack stack create -t server.yaml assignment2-task3-stack \
 --parameter "name=assignment2-task3-vm" \
+--parameter "router=cc17-router" \
+--parameter "server=cc17-server" \
+--parameter "network=cc17-net"	\
+--parameter "subnet=cc17-subnet" \
 --parameter "key_pair=group06key" \
---parameter "image=ubuntu-16.04" \
---parameter "network=cc17-net"
