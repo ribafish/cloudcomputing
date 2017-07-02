440be33892ff0310633bdecb7471d25f1b8cfd1c
Gasper Kojek <gasper.kojek@gmail.com>
2017-07-02 05:54:34 +0200

    Renamed the files to conform to the instructions

--- assignment2/task3/create-stack3.sh
+++ assignment2/task3/create-stack3.sh
@@ -1,4 +1,4 @@
-openstack stack create -t server.yaml assignment2-task3-stack \
+openstack stack create -t server-landscape.yaml assignment2-task3-stack \
 --parameter "name=assignment2-task3-vm" \
 --parameter "router=cc17-23-router" \
 --parameter "server=cc17-23-server" \
